6d6bcca041e9a46f6630997312bdfa50769f2c5a
t-yuntaoli <t-yuntaoli@microsoft.com>
2021-03-21 01:56:04 +0800

    sentencepiece post training - 0321

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,14 @@
 from models.bert_cls_cl_v6 import BertCls
 from models.bert_eot import BertEOT
 from models.bert_insertion import BertInsertion
+from models.bert_cls_moe import BertClsMoe
 
 
 def Model(hparams, *args):
     name_model_map = {
         "bert_base": BertCls,
         "bert_post": BertCls,
+        "bert_post_moe": BertClsMoe,
 
         "electra_base": BertCls,
         "electra_post": BertCls,
